5c6f7888e83cbb900a832f1be125e118e3cd0ad7
fengwu2004 <user@users-imac-2.local>
2015-11-12 10:37:37 +0800

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by user on 15/11/12.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
